a3576014d389156597fc0c2ed2852f6796756c26
IS <ivoseeba@gmail.com>
2021-03-13 21:21:16 +0200

    messages

---
+++ routes.py
@@ -0,0 +1,59 @@
+from app import app
+import visits
+from flask import render_template, request, redirect
+import messages
+import users
+
+
+@app.route("/")
+def index():
+    list = messages.get_list()
+    visits.add_visit()
+    counter = visits.get_counter()
+    return render_template("index.html", counter=counter, count=len(list), messages=list)
+
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+
+@app.route("/send", methods=["post"])
+def send():
+    content = request.form["content"]
+    if messages.send(content):
+        return redirect("/")
+    else:
+        return render_template("error.html", message="Viestin lähetys ei onnistunut")
+
+
+@app.route("/login", methods=["get", "post"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+
+@app.route("/register", methods=["get", "post"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.register(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
